7001797e8ed92c372cf3dfafb160851fe66afb89
Keith Hodo <keith.d.hodo@avanade.com>
2022-03-21 16:50:12 -0700

    Updates (#2)

---
+++ Source/AzureEventHubDemo.Core/Models/DeviceReading.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="DeviceReading.cs" company="Keith D. Hodo">
+// Copyright (c) Keith D. Hodo. All rights reserved.
+// </copyright>
+
+namespace AzureEventHubDemo.Core.Models
+{
+    using System.Text.Json.Serialization;
+
+    public class DeviceReading
+    {
+        [JsonPropertyName("deviceId")]
+        public string DeviceId { get; set; }
+
+        [JsonPropertyName("deviceTemperature")]
+        public decimal DeviceTemperature { get; set; }
+
+        [JsonPropertyName("damageLevel")]
+        public string DamageLevel { get; set; }
+
+        [JsonPropertyName("deviceAgeInDays")]
+        public int DeviceAgeInDays { get; set; }
+    }
+}
