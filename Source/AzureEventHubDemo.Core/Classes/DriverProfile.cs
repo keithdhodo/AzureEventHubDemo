826f6223493e3673d937e1bb5fb156de4530c5ac
Keith Hodo <keith.d.hodo@avanade.com>
2022-03-21 14:02:48 -0700

    Adding initial files so I can share

---
+++ Source/AzureEventHubDemo.Core/Classes/DriverProfile.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="DriverProfile.cs" company="Keith D. Hodo">
+// Copyright (c) Keith D. Hodo. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace AzureEventHubDemo.Writer.Classes
+{
+    [Serializable]
+    public class DriverProfile
+    {
+        /// <summary>
+        /// Driver Full Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Driver's Followers
+        /// </summary>
+        public int Followers { get; set; }
+
+        /// <summary>
+        /// Driver's Area
+        /// </summary>
+        public string Area { get; set; }
+
+        /// <summary>
+        /// Driver's Bio
+        /// </summary>
+        public string Bio { get; set; }
+    }
+}
